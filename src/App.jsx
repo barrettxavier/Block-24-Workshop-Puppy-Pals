b44ddc27a1b681d456ad8afa84d4c391dd89be6c
barrettxavier <barrettxavier@yahoo.com>
2023-11-13 15:43:15 -0500

    successful deploy to netlify

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,30 @@ import './App.css'
 
 function App() {
 
-  // console.log(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
     <>
       {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
         })
       }
+      
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   )
 }
