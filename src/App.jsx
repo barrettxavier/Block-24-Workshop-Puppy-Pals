aea526b89753059fe09248d23f64f6cd2c428f5a
barrettxavier <barrettxavier@yahoo.com>
2023-11-13 14:59:26 -0500

    Mapped over puppy array and return the puppy.name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import { puppyList } from './data.js'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  // console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
+
   return (
     <>
-  
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </>
   )
 }
